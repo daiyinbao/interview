a71016185cf22dfb7476f1a6bf9a6b8675c0a025
昭银 <3375461788@qq.com>
2026-03-03 23:27:07 +0800

    add:可恢复 + 可重试 + 分布式安全 的任务状态推进协议接口

---
+++ 05_acync_task/src/main/java/com/dyb/asyncscheduler/store/TaskStore.java
@@ -0,0 +1,35 @@
+/**
+ * @author dai_yin_bao
+ * @date 2026/3/3 23:13
+ * @file TaskStore.java
+ */
+package com.dyb.asyncscheduler.store;
+
+import com.dyb.asyncscheduler.task.Task;
+import com.dyb.asyncscheduler.task.TaskState;
+
+import java.util.Optional;
+
+/**
+ * 在“多线程 + 多进程 + 崩溃恢复”的环境下安全地推进任务状态机
+ */
+public interface TaskStore {
+    //创建任务
+    void insert(Task task);
+
+    //查询任务
+    Optional<Task> get(String taskId);
+
+    //保证同一时间只有一个 worker 能执行任务
+    boolean tryLease(String taskId, String owner,long nowMs,long ttlMs);
+
+    //推进状态
+    boolean markRunning(String taskId, TaskState expectedState);
+
+    void completeSuccess(String taskId);
+
+    void completeFailure(String taskId, String error, boolean retryable,long nextRunAtEpochMs);
+
+    void releaseLease(String taskId, String owner);
+
+}
